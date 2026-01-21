578ab7d74acfcebd60152f4b050bd143a9db9d9e
mmoo758 <yccao94@gmail.com>
2026-01-21 22:36:41 +0800

    Revert "build system cleanups (#36202) and more"
    This reverts commit cbf3d51d731a91b9de815de7c3b74fb376826f01.
    
    Reapply "build system cleanups (#36202) and more"
    
    This reverts commit 9646b6762e1a2a414e257d76efacd4a49887e7ae.
    
    Revert ""build system cleanups (#36202) and more""
    
    This reverts commit 71cd45ee85ff807fb277b0b256d7a58e65d8024f.
    
    Revert "修复"
    
    This reverts commit e1e1baba6424442eecff6f7c12e8ec919a97c0a8.

--- sunnypilot/modeld/thneed/serialize.cc
+++ sunnypilot/modeld/thneed/serialize.cc
@@ -54,7 +54,7 @@ void Thneed::load(const char *filename) {
       desc.image_height = mobj["height"].int_value();
       desc.image_row_pitch = mobj["row_pitch"].int_value();
       assert(sz == desc.image_height*desc.image_row_pitch);
-#ifdef __TICI__
+#ifdef QCOM2
       desc.buffer = clbuf;
 #else
       // TODO: we are creating unused buffers on PC
@@ -66,7 +66,7 @@ void Thneed::load(const char *filename) {
 
       cl_int errcode;
 
-#ifndef __TICI__
+#ifndef QCOM2
       if (mobj["needs_load"].bool_value()) {
         clbuf = clCreateImage(context, CL_MEM_COPY_HOST_PTR | CL_MEM_READ_WRITE, &format, &desc, &buf[ptr-sz], &errcode);
       } else {
