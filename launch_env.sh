c29adbe51559882ab72b083b7737f3ddedf949f5
mmoo758 <yccao94@gmail.com>
2026-01-30 12:05:27 +0800

    Revert "agnos15.1"
    This reverts commit c2e7b0aa18dd26c8bd3425e503867b0980ac932e.

--- launch_env.sh
+++ launch_env.sh
@@ -11,7 +11,7 @@ export API_HOST='http://op.caminfo.cn:7777'
 export MAPBOX_TOKEN='sk.eyJ1IjoibWF3ZWl5dXdlaXdlaSIsImEiOiJjbG15NHN1dDMwdWc5MmxwaDdkZ3Z5dHNyIn0.pk06qTKkAZyBC1Z37v8i0A'
 
 if [ -z "$AGNOS_VERSION" ]; then
-  export AGNOS_VERSION="15.1"
+  export AGNOS_VERSION="13.1"
 fi
 
 export STAGING_ROOT="/data/safe_staging"
