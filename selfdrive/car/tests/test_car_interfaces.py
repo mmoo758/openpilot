fd6795c6ac50f09ed42661d0013475b9dca09d4d
mmoo758 <yccao94@gmail.com>
2026-03-19 13:38:03 +0800

    change 1#

--- selfdrive/car/tests/test_car_interfaces.py
+++ selfdrive/car/tests/test_car_interfaces.py
@@ -60,7 +60,7 @@ class TestCarInterfaces:
     # Test controller initialization
     # TODO: wait until card refactor is merged to run controller a few times,
     #  hypothesis also slows down significantly with just one more message draw
-    LongControl(car_params, car_params_sp)
+    LongControl(car_params)
     if car_params.steerControlType == CarParams.SteerControlType.angle:
       LatControlAngle(car_params, car_params_sp, car_interface, DT_CTRL)
     elif car_params.lateralTuning.which() == 'pid':
