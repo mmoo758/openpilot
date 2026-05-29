27d86580350f35802671fcbcdc6e9cceb7f4cae0
mmoo758 <yccao94@gmail.com>
2026-05-29 13:35:48 +0800

    重新修改测试beep

--- selfdrive/selfdrived/beep.py
+++ selfdrive/selfdrived/beep.py
@@ -72,8 +72,10 @@ class Beepd:
         #self.dispatch_beep(self.engage)
       #elif new_alert == AudibleAlert.disengage:
         #self.dispatch_beep(self.disengage)
-      if new_alert in [AudibleAlert.refuse, AudibleAlert.prompt, AudibleAlert.warningSoft]:
+      if new_alert in [AudibleAlert.warningSoft, AudibleAlert.warningImmediate]:
         self.dispatch_beep(self.warning)
+      elif new_alert == AudibleAlert.promptRepeat:
+        self.dispatch_beep(self.engage)
 
   def get_audible_alert(self, sm):
     if sm.updated['selfdriveState']:
